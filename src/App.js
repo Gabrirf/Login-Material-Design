416fc4277f8f2a3291cc90f9bd13ccc03e2a1e02
Gabri <gabrodflo@alum.us.es>
2019-01-28 12:56:46 +0100

    Add card style to login container
    Move logo to Login component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { MDBCol, MDBContainer, MDBRow } from "mdbreact";
 import Login from "./components/Login";
 import "./index.css";
-import logo from "./logo.jpeg";
 
 class App extends Component {
   render() {
@@ -10,9 +9,6 @@ class App extends Component {
       <MDBContainer>
         <MDBRow center style={{ height: "100vh" }}>
           <MDBCol middle="true" sm="8" >
-            <div class="text-center">
-              <img src={logo} alt="logo" style={{ width: "10rem" }} />
-            </div>
             <Login />
           </MDBCol>
         </MDBRow>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,39 @@
 import React from "react";
-import { MDBInput, MDBBtn } from 'mdbreact';
+import { MDBCard, MDBCardBody, MDBBtn } from 'mdbreact';
+import logo from "../logo.jpeg";
 
 const FormPage = () => {
   return (
-    <form>
-        <p className="h4 text-center mb-4">Sign in</p>
-        <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
-            Your email
-        </label>
-        <input
-            type="email"
-            id="defaultFormLoginEmailEx"
-            className="form-control"
-        />
-        <br />
-        <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
-            Your password
-        </label>
-        <input
-            type="password"
-            id="defaultFormLoginPasswordEx"
-            className="form-control"
-        />
-        <div className="text-center mt-4">
-            <MDBBtn color="indigo" type="submit">Login</MDBBtn>
-        </div>
-    </form>
+    <MDBCard>
+        <MDBCardBody>
+            <div class="text-center">
+              <img src={logo} alt="logo" style={{ width: "10rem" }} />
+            </div>
+            <form>
+                <p className="h4 text-center mb-4">Sign in</p>
+                <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+                    Your email
+                </label>
+                <input
+                    type="email"
+                    id="defaultFormLoginEmailEx"
+                    className="form-control"
+                />
+                <br />
+                <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+                    Your password
+                </label>
+                <input
+                    type="password"
+                    id="defaultFormLoginPasswordEx"
+                    className="form-control"
+                />
+                <div className="text-center mt-4">
+                    <MDBBtn color="indigo" type="submit">Login</MDBBtn>
+                </div>
+            </form>
+        </MDBCardBody>
+    </MDBCard>
   );
 };
 
