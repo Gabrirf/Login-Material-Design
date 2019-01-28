5d4f8e5b8c6834e92bc8549bc13301780464f988
Gabri <gabrodflo@alum.us.es>
2019-01-28 12:44:10 +0100

    Another form using HTML tags without MDB Component
    Another style to implement forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,30 +3,29 @@ import { MDBInput, MDBBtn } from 'mdbreact';
 
 const FormPage = () => {
   return (
-          <form>
-            <p className="h5 text-center mb-4">Sign in</p>
-            <div className="grey-text">
-              <MDBInput
-                label="Type your email"
-                icon="envelope"
-                group
-                type="email"
-                validate
-                error="wrong"
-                success="right"
-              />
-              <MDBInput
-                label="Type your password"
-                icon="lock"
-                group
-                type="password"
-                validate
-              />
-            </div>
-            <div className="text-center">
-              <MDBBtn>Login</MDBBtn>
-            </div>
-          </form>
+    <form>
+        <p className="h4 text-center mb-4">Sign in</p>
+        <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+            Your email
+        </label>
+        <input
+            type="email"
+            id="defaultFormLoginEmailEx"
+            className="form-control"
+        />
+        <br />
+        <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+            Your password
+        </label>
+        <input
+            type="password"
+            id="defaultFormLoginPasswordEx"
+            className="form-control"
+        />
+        <div className="text-center mt-4">
+            <MDBBtn color="indigo" type="submit">Login</MDBBtn>
+        </div>
+    </form>
   );
 };
 
