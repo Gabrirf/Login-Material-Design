5a695b2e74d0a985cc3e3bf120d03e2c318f4e14
Gabri <gabrodflo@alum.us.es>
2019-01-28 13:16:40 +0100

    Add card style to login container
    Moved logo to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,40 @@
 import React from "react";
-import { MDBInput, MDBBtn } from 'mdbreact';
+import { MDBInput, MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
+import logo from "../logo.jpeg";
 
 const FormPage = () => {
   return (
-          <form>
-            <p className="h5 text-center mb-4">Sign in</p>
-            <div className="grey-text">
-              <MDBInput
-                label="Type your email"
-                icon="envelope"
-                group
-                type="email"
-                validate
-                error="wrong"
-                success="right"
-              />
-              <MDBInput
-                label="Type your password"
-                icon="lock"
-                group
-                type="password"
-                validate
-              />
+    <MDBCard>
+        <MDBCardBody>
+            <div class="text-center">
+              <img src={logo} alt="logo" style={{ width: "10rem" }} />
             </div>
-            <div className="text-center">
-              <MDBBtn>Login</MDBBtn>
-            </div>
-          </form>
+            <form>
+                <p className="h5 text-center mb-4">Sign in</p>
+                <div className="grey-text">
+                    <MDBInput
+                    label="Type your email"
+                    icon="envelope"
+                    group
+                    type="email"
+                    validate
+                    error="wrong"
+                    success="right"
+                    />
+                    <MDBInput
+                    label="Type your password"
+                    icon="lock"
+                    group
+                    type="password"
+                    validate
+                    />
+                </div>
+                <div className="text-center">
+                    <MDBBtn>Login</MDBBtn>
+                </div>
+            </form>
+        </MDBCardBody>
+    </MDBCard>
   );
 };
 
