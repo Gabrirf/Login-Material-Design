5a99967add50e42fbdda33665c2310e52cdbf04b
Gabri <gabrodflo@alum.us.es>
2019-01-28 13:57:12 +0100

    Add simple Navbar Component

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBIcon, MDBContainer } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class NonFixedNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <header>
+            <MDBNavbar color="default-color" dark expand="md">
+              <MDBNavbarBrand href="/">
+                <strong>Espacios medioambientales</strong>
+              </MDBNavbarBrand>
+              <MDBNavbarToggler onClick={this.onClick} />
+              <MDBCollapse isOpen={this.state.collapse} navbar>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><strong>Menu </strong><MDBIcon icon="user" /></MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </header>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default NonFixedNavbar;
